2b2409149c998c8d514b0dd35a1e185c8696491d
bekalimuminov <bekalimuminov@gmail.com>
2025-09-15 17:31:06 +0500

    imtihon

---
+++ index.js
@@ -0,0 +1,49 @@
+// let son = prompt("son kiriting")*1
+// let son2= prompt("son kiriting")*1
+// let amal = prompt("amal kiriting")
+// if (amal ==="+" ) {
+//     console.log(son + son2)
+// }else if (amal === "/" ) {
+// console.log(son / son2)
+// }else if (amal === "*") {
+//     console.log(son * son2)
+// }else if (amal === "%") {
+//     console.log(son % son2)
+// }else if (amal === "-") {
+//     console.log(son - son2)
+// }
+
+// let son = prompt("son kirit")
+// if (son % 2 === 0) {
+//     console.log("juft son")
+// }else {
+//     console.log("toq son")
+// }
+
+// let son = "231"
+// let son2=0
+// for (let i = 0; i < son.length; i++) {
+//
+//     son2 += son[i]*1
+// }
+// console.log(son2)
+
+// let massiv = [12 ,50,5445564,5,4,154]
+// const Double = massiv.find((number) =>{
+//     return number >= 100
+// })
+// console.log(Double)
+
+// let massiv = [5,4,5,54,84]
+// let engkatta = massiv[0]
+// let enkichik = massiv[0]
+// massiv.forEach(item => {
+//     if (item > engkatta) {
+//         engkatta = item
+//     }
+//     if (item < enkichik) {
+//         enkichik = item
+//     }
+// })
+// console.log(enkichik)
+// console.log(engkatta)
